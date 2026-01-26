3c0bcb81ab538f7ccd680a27cbbdd3c00e856657
viacheslav4734646 <viacheslav@horda.dev>
2026-01-26 17:21:04 +0200

    update list view changes, add query changes

--- lib/src/view.g.dart
+++ lib/src/view.g.dart
@@ -9,26 +9,26 @@ part of 'view.dart';
 ListPageItemAdded _$ListPageItemAddedFromJson(Map<String, dynamic> json) =>
     ListPageItemAdded(
       pageId: json['pageId'] as String,
-      key: json['key'] as String,
-      value: json['value'] as String,
+      pos: (json['pos'] as num).toDouble(),
+      refId: json['refId'] as String,
     );
 
 Map<String, dynamic> _$ListPageItemAddedToJson(ListPageItemAdded instance) =>
     <String, dynamic>{
       'pageId': instance.pageId,
-      'key': instance.key,
-      'value': instance.value,
+      'pos': instance.pos,
+      'refId': instance.refId,
     };
 
 ListPageItemRemoved _$ListPageItemRemovedFromJson(Map<String, dynamic> json) =>
     ListPageItemRemoved(
       pageId: json['pageId'] as String,
-      key: json['key'] as String,
+      pos: (json['pos'] as num).toDouble(),
     );
 
 Map<String, dynamic> _$ListPageItemRemovedToJson(
   ListPageItemRemoved instance,
-) => <String, dynamic>{'pageId': instance.pageId, 'key': instance.key};
+) => <String, dynamic>{'pageId': instance.pageId, 'pos': instance.pos};
 
 ListPageCleared _$ListPageClearedFromJson(Map<String, dynamic> json) =>
     ListPageCleared(pageId: json['pageId'] as String);
